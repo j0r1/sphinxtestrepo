dc02a7fc2667ae29c3def862bf4f47bcfacc5a3c
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2016-10-13 23:03:50 +0200

    .

--- sphinxdoc/source/conf.py
+++ sphinxdoc/source/conf.py
@@ -141,8 +141,8 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_theme = 'sphinx_rtd_theme-mod'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path(), "."]
 
 #html_theme_options = {
 #    "rightsidebar": "true",
