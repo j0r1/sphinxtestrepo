ce942fce5f9111ef12efa704ba456f2bb23b4abb
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2016-10-14 13:39:37 +0200

    .

--- sphinxdoc/source/conf.py
+++ sphinxdoc/source/conf.py
@@ -139,6 +139,11 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 #html_theme = "classic-mod"
 # html_theme_path = [ "." ]
 
+print("Current theme: {}".format(html_theme))
+print("Current theme path: {}".format(html_theme_path))
+import sys
+print("sys.path = {}".format(sys.path))
+
 html_theme = 'sphinx_rtd_theme-mod'
 html_theme_path = [ "." ]
 
