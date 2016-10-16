18b20e7aa671de35caaf5f1e713ee38fe8b8680b
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2016-10-16 17:18:40 +0200

    .

--- sphinxdoc/source/conf.py
+++ sphinxdoc/source/conf.py
@@ -142,10 +142,10 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 #if not on_rtd:
 #    html_theme = 'sphinx_rtd_theme'
 
-#html_context = {
-#        'extra_css_files': [ "_static/theme-mod.css" ],
+html_context = {
+        'extra_css_files': [ "_static/theme-mod.css" ],
 #        'script_files': [ "_static/inspectlinks.js" ],
-#}
+}
 
 #html_theme_path = [ "." ]
 
